a68a4ffbfc1873b50a09091a38fbcdfd74b69f72
Breno Medeiros De Oliveira <>
2023-07-30 03:49:34 -0300

    - Adicao de atalho para pasta do excel csv e do txt da minhaCarteira - Tudo feito exceto - TODO Corrigir Bug em doisFiisConsecutivosEntrando - TODO Corrigir Bug em doisFiisConsecutivosSaindo - TODO Implementar Testes doisFiisNaoConsecutivosEntrando e doisFiisNaoConsecutivosSaindo

--- app.js
+++ app.js
@@ -5,16 +5,44 @@
 function testeJS(a){
 
 
-	var carteriaRodrigo = "HGRE11 LINK JSRE11 LINK BROF11 LINK BLMR11 LINK RFOF11 LINK HGFF11 LINK FIIB11 LINK XPIN11 LINK BTLG11 LINK HLOG11 LINK LVBI11 LINK GALG11 LINK RBRL11 LINK LGCP11 LINK AFHI11 LINK BARI11 LINK PLCR11 LINK KCRE11 LINK HSAF11 LINK WHGR11 LINK HGRU11 LINK TRXF11 LINK FIGS11 LINK MALL11 LINK HGBS11 LINK PQDP11   Setor Cotação Cotas Valor Médio   Lajes 134,64 39 150,37   Valor Atual R$ 5.250,96   R$ 6.975,00 R$ 8.364,00 R$ 7.804,26 R$ 7.881,12 R$ 7.711,20 R$ 8.238,71 R$ 7.198,20 R$ 8.119,20 R$ 8.123,57 R$ 13.800,00 R$ 6.188,00 R$ 11.594,00 R$ 9.730,60 R$ 6.292,65 R$ 7.059,36 R$ 8.262,46 R$ 10.685,27 R$ 7.866,00 R$ 8.262,00 R$ 10.397,40 R$ 9.441,00 R$ 11.426,58 R$ 12.399,84 R$ 14.700,00 R$ 3.660,00   Peso Status 2,31% Manutenção   3,07% Compra 3,68% Compra 3,43% Manutenção 3,47% Compra 3,39% Compra 3,62% Compra   3,16% Compra 3,57% Compra 3,57% Compra 6,07% Compra 2,72% Compra 5,10% Compra 4,28% Compra 2,77% Manutenção 3,10% Compra 3,63% Compra 4,70% Compra 3,46% Compra 3,63% Compra 4,57% Compra 4,15% Compra 5,02% Compra 5,45% Compra 6,46% Compra   1,61% Manutenção   2   3   4   5   6   7   8   9   10   11 08/08/21   Lajes Lajes FoF FoF FoF   77,5 90 69,7 120 6,81 1146   75,78 104   87,68 61,82 7,52 72,83 76,13   10/11/20 21/5/23 22/08/21 16/08/20 25/09/22 19/01/20 12/04/20 23/08/20 21/12/20   85,68 90   Industrial 484,63 17 561,94   24/04/21 20/02/22 14/08/22 06/06/21 19/01/20 20/12/20 08/01/23 23/04/23 01/05/23   Logístico 9,1 680   11,13 82,86 76,32 95,77 100,71 86,95 86,1 76,6 8,73 127,66 102,68 70,55 103,53 179,16 3.721,00   12   13   14   15   16   17   18   19   20   21 19/01/20   artemisebc@gmail.com   22 23 24 25 26   05/07/20 19/01/20 14/5/23 24/7/22 19/01/20   Logístico   Logístico   Recebíveis   Recebíveis   Recebíveis   Recebíveis   Recebíveis   Hedge Fund   R. Urbana R. Urbana Shopping Shopping Shopping";
+	var rodrigoCarteria = "";
+	var minhaCarteria = "";
+
+
+	if (true) {				// Cenario carteirasSaoMesmoTamanho e carteirasSaoIdenticas
+		rodrigoCarteria = "HGRE11 JSRE11 BROF11 BLMR11 RFOF11 HGFF11 FIIB11 XPIN11 BTLG11 HLOG11 LVBI11 GALG11 RBRL11 LGCP11 AFHI11 BARI11 PLCR11 KCRE11 HSAF11 WHGR11 HGRU11 TRXF11 FIGS11 MALL11 HGBS11 PQDP11 2,31% 3,07% 3,68% 3,43% 3,47% 3,39% 3,62% 3,16% 3,57% 3,57% 6,07% 2,72% 5,10% 4,28% 2,77% 3,10% 3,63% 4,70% 3,46% 3,63% 4,57% 4,15% 5,02% 5,45% 6,46% 1,61% Manutenção Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção";
+		minhaCarteria = "AFHI11 BARI11 BLMR11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+	} else if (false) { 		// Cenario carteirasSaoMesmoTamanho e !carteirasSaoIdenticas
+		rodrigoCarteria = "HGRE11 JSRE11 BROF11 BLMR11 RFOF11 HGFF11 FIIB11 XPIN11 BTLG11 HLOG11 LVBI11 GALG11 RBRL11 LGCP11 AFHI11 BARI11 PLCR11 KCRE11 HSAF11 WHGR11 HGRU11 TRXF11 FIGS11 MALL11 HGBS11 PQDP11 2,31% 3,07% 3,68% 3,43% 3,47% 3,39% 3,62% 3,16% 3,57% 3,57% 6,07% 2,72% 5,10% 4,28% 2,77% 3,10% 3,63% 4,70% 3,46% 3,63% 4,57% 4,15% 5,02% 5,45% 6,46% 1,61% Manutenção Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção";
+		minhaCarteria = "AFHI11 BLMR11 BREN11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+	} else if (false) { 		// Cenario !carteirasSaoMesmoTamanho e !carteirasSaoIdenticas e rodrigoCarteriaTemFundosAMais>0
+		rodrigoCarteria = "HGRE11 JSRE11 BROF11 BLMR11 RFOF11 HGFF11 FIIB11 XPIN11 BTLG11 HLOG11 LVBI11 GALG11 RBRL11 LGCP11 AFHI11 BARI11 PLCR11 KCRE11 HSAF11 WHGR11 HGRU11 TRXF11 FIGS11 MALL11 HGBS11 PQDP11 2,31% 3,07% 3,68% 3,43% 3,47% 3,39% 3,62% 3,16% 3,57% 3,57% 6,07% 2,72% 5,10% 4,28% 2,77% 3,10% 3,63% 4,70% 3,46% 3,63% 4,57% 4,15% 5,02% 5,45% 6,46% 1,61% Manutenção Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção";
+		minhaCarteria = "AFHI11 BLMR11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+	} else if (false) { 		// Cenario !carteirasSaoMesmoTamanho e !carteirasSaoIdenticas e rodrigoCarteriaTemFundosAMais>0 e doisFiisConsecutivosEntrando
+		rodrigoCarteria = "HGRE11 JSRE11 BROF11 BLMR11 RFOF11 HGFF11 FIIB11 XPIN11 BTLG11 HLOG11 LVBI11 GALG11 RBRL11 LGCP11 AFHI11 BARI11 PLCR11 KCRE11 HSAF11 WHGR11 HGRU11 TRXF11 FIGS11 MALL11 HGBS11 PQDP11 2,31% 3,07% 3,68% 3,43% 3,47% 3,39% 3,62% 3,16% 3,57% 3,57% 6,07% 2,72% 5,10% 4,28% 2,77% 3,10% 3,63% 4,70% 3,46% 3,63% 4,57% 4,15% 5,02% 5,45% 6,46% 1,61% Manutenção Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção";
+		minhaCarteria = "AFHI11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+	} else if (false) { 		// Cenario !carteirasSaoMesmoTamanho e !carteirasSaoIdenticas e rodrigoCarteriaTemFundosAMais<0
+		rodrigoCarteria = "HGRE11 JSRE11 BROF11 BLMR11 RFOF11 HGFF11 FIIB11 XPIN11 BTLG11 HLOG11 LVBI11 GALG11 RBRL11 LGCP11 AFHI11 PLCR11 KCRE11 HSAF11 WHGR11 HGRU11 TRXF11 FIGS11 MALL11 HGBS11 PQDP11 3,07% 3,68% 3,43% 3,47% 3,39% 3,62% 3,16% 3,57% 3,57% 6,07% 2,72% 5,10% 4,28% 2,77% 3,10% 3,63% 4,70% 3,46% 3,63% 4,57% 4,15% 5,02% 5,45% 6,46% 1,61% Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção";
+		minhaCarteria = "AFHI11 BARI11 BLMR11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+	} else if (false) { 		// Cenario !carteirasSaoMesmoTamanho e !carteirasSaoIdenticas e rodrigoCarteriaTemFundosAMais<0 e doisFiisConsecutivosSaindo
+		rodrigoCarteria = "HGRE11 JSRE11 BROF11 BLMR11 RFOF11 HGFF11 FIIB11 XPIN11 BTLG11 HLOG11 LVBI11 GALG11 RBRL11 LGCP11 AFHI11 PLCR11 KCRE11 HSAF11 WHGR11 HGRU11 TRXF11 FIGS11 MALL11 HGBS11 PQDP11 3,07% 3,68% 3,43% 3,47% 3,39% 3,62% 3,16% 3,57% 3,57% 6,07% 2,72% 5,10% 4,28% 2,77% 3,10% 3,63% 4,70% 3,46% 3,63% 4,57% 4,15% 5,02% 5,45% 6,46% 1,61% Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção Compra Compra Compra Compra Compra Compra Compra Compra Compra Compra Manutenção";
+		minhaCarteria = "AFHI11 BARI11 BLMR11 BREN11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+	}
+
+	//var rodrigoCarteria = "HGRE11 LINK JSRE11 LINK BROF11 LINK BLMR11 LINK RFOF11 LINK HGFF11 LINK FIIB11 LINK XPIN11 LINK BTLG11 LINK HLOG11 LINK LVBI11 LINK GALG11 LINK RBRL11 LINK LGCP11 LINK AFHI11 LINK BARI11 LINK PLCR11 LINK KCRE11 LINK HSAF11 LINK WHGR11 LINK HGRU11 LINK TRXF11 LINK FIGS11 LINK MALL11 LINK HGBS11 LINK PQDP11   Setor Cotação Cotas Valor Médio   Lajes 134,64 39 150,37   Valor Atual R$ 5.250,96   R$ 6.975,00 R$ 8.364,00 R$ 7.804,26 R$ 7.881,12 R$ 7.711,20 R$ 8.238,71 R$ 7.198,20 R$ 8.119,20 R$ 8.123,57 R$ 13.800,00 R$ 6.188,00 R$ 11.594,00 R$ 9.730,60 R$ 6.292,65 R$ 7.059,36 R$ 8.262,46 R$ 10.685,27 R$ 7.866,00 R$ 8.262,00 R$ 10.397,40 R$ 9.441,00 R$ 11.426,58 R$ 12.399,84 R$ 14.700,00 R$ 3.660,00   Peso Status 2,31% Manutenção   3,07% Compra 3,68% Compra 3,43% Manutenção 3,47% Compra 3,39% Compra 3,62% Compra   3,16% Compra 3,57% Compra 3,57% Compra 6,07% Compra 2,72% Compra 5,10% Compra 4,28% Compra 2,77% Manutenção 3,10% Compra 3,63% Compra 4,70% Compra 3,46% Compra 3,63% Compra 4,57% Compra 4,15% Compra 5,02% Compra 5,45% Compra 6,46% Compra   1,61% Manutenção   2   3   4   5   6   7   8   9   10   11 08/08/21   Lajes Lajes FoF FoF FoF   77,5 90 69,7 120 6,81 1146   75,78 104   87,68 61,82 7,52 72,83 76,13   10/11/20 21/5/23 22/08/21 16/08/20 25/09/22 19/01/20 12/04/20 23/08/20 21/12/20   85,68 90   Industrial 484,63 17 561,94   24/04/21 20/02/22 14/08/22 06/06/21 19/01/20 20/12/20 08/01/23 23/04/23 01/05/23   Logístico 9,1 680   11,13 82,86 76,32 95,77 100,71 86,95 86,1 76,6 8,73 127,66 102,68 70,55 103,53 179,16 3.721,00   12   13   14   15   16   17   18   19   20   21 19/01/20   artemisebc@gmail.com   22 23 24 25 26   05/07/20 19/01/20 14/5/23 24/7/22 19/01/20   Logístico   Logístico   Recebíveis   Recebíveis   Recebíveis   Recebíveis   Recebíveis   Hedge Fund   R. Urbana R. Urbana Shopping Shopping Shopping";
+	//var minhaCarteria = "AFHI11 BLMR11 BRENO11 BROF11 BTLG11 FIGS11 FIIB11 GALG11 HGBS11 HGFF11 HGRE11 HGRU11 HLOG11 HSAF11 JSRE11 KCRE11 LGCP11 LVBI11 MALL11 PLCR11 PQDP11 RBRL11 RFOF11 TRXF11 WHGR11 XPIN11";
+
+
 
 	// Obter todos os links da página
-	var array = carteriaRodrigo.split(" ");
+	var array = rodrigoCarteria.split(" ");
+	var arrayMeu = minhaCarteria.split(" ");
 
-	// initialize array
+	// initialize arraysRodrigo
 	var arrFundo = [];
 	var arrRepresentatividade = [];
 	var arrStatus = [];
-	// append new value to the array
+	// append new value to the arraysRodrigo
 
 
 	for (let element of array) {
@@ -22,7 +50,11 @@ function testeJS(a){
 			arrFundo.push(element);
 			//console.log(element);
 		} else if(element=="Compra" || element=="Venda" || element=="Manutenção") {
-			arrStatus.push(element);
+			if (element=="Manutenção") {
+				arrStatus.push("Manutencao");
+			} else {
+				arrStatus.push(element);
+			}
 			//console.log(element);
 		} else if(element.endsWith("%") && element.length<=6) {
 			arrRepresentatividade.push(element);
@@ -39,20 +71,282 @@ function testeJS(a){
 		arr.push(montadorDeLinha);
 		montadorDeLinha = "";
 	}
-	arr = arr.sort();
+	var arrayRodrigo = arr.sort();
 	//console.log(arr);
 	// Chamar preenchimento para concluir
-	//completion(arr.join("\r\n"));
+	//completion(arrayRodrigo.join("\r\n"));
+
+
+	if (arrayMeu.length!=0) {
+		//Verificacao se sao do mesmo tamanho
+		var carteirasSaoMesmoTamanho = (arrayRodrigo.length)==(arrayMeu.length);
+
+		//Verificacao se sao identicas
+		var carteirasSaoIdenticas = false;
+		if (carteirasSaoMesmoTamanho) {
+			carteirasSaoIdenticas = true;
+			for (var i = 0; i < arrayRodrigo.length; i++) {
+				if ((arrayRodrigo[i].split(";")[0])!=arrayMeu[i]) {
+					carteirasSaoIdenticas = false;
+					break;
+				}
+			}
+		}
+
+		//Criacao de array comparativo com as duas carteiras
+		var arrayComAsDuasCarteiras = [];
+		if (!carteirasSaoIdenticas) {
+			var montadorDeLinha = "";
+
+			//Rodrigo vendeu e comprou X fundos ->               V = C
+			if(carteirasSaoMesmoTamanho) {
+
+				for (var i = 0; i < arrayRodrigo.length; i++) {
+
+					//Verificacao se eh um fundo que existia e continua na carteira
+					if (arrayMeu[i]==(arrayRodrigo[i].split(";")[0])) {
+						montadorDeLinha += arrayMeu[i];
+						montadorDeLinha += ";";
+						montadorDeLinha += arrayRodrigo[i];
+						arrayComAsDuasCarteiras.push(montadorDeLinha);
+						montadorDeLinha = "";
+
+					} else {
+						//Verificacao se ele vendeu arrayMeu[i]
+						if ((arrayMeu).find(element => element==(arrayRodrigo[i].split(";")[0]))) {
+							montadorDeLinha += arrayMeu[i];
+							montadorDeLinha += ";";
+							montadorDeLinha += "(-)";
+							montadorDeLinha += ";";
+							montadorDeLinha += "Venda";
+							montadorDeLinha += ";";
+							montadorDeLinha += "0.00%";
+							arrayComAsDuasCarteiras.push(montadorDeLinha);
+							montadorDeLinha = "";
+
+						//se nao, entao ele comprou o novo arrayRodrigo[i]
+						} else {
+							montadorDeLinha += "(-)";
+							montadorDeLinha += ";";
+							montadorDeLinha += arrayRodrigo[i];
+							arrayComAsDuasCarteiras.push(montadorDeLinha);
+							montadorDeLinha = "";
+
+						}
+					}
+				}
+
+			} else {
+				var rodrigoCarteriaTemFundosAMais = (arrayRodrigo.length) - (arrayMeu.length);
+
+				var tempArrayRodrigo = [];
+				for (var i = 0; i < arrayRodrigo.length; i++) {
+					tempArrayRodrigo.push((arrayRodrigo[i].split(";")[0]));
+				}
+
+				//Rodrigo Vendeu menos fundos do que Comprou ->  V < C
+				if (rodrigoCarteriaTemFundosAMais > 0) {
+					var novosFiisApenasDoRodrigo = [];
+					var arrayNovoMeu = [];
+					for (var i = 0; i < arrayRodrigo.length; i++) {
+						if ((arrayRodrigo[i].split(";")[0]) != "(-)") {
+							if ((arrayMeu).find(element => element==(arrayRodrigo[i].split(";")[0]))) {
+								//arrayNovoMeu.push(arrayRodrigo[i].split(";")[0])
+							} else {
+								//arrayNovoMeu.push("(-)");
+								novosFiisApenasDoRodrigo.push((arrayRodrigo[i].split(";")[0]));
+							}
+						}
+					}
+
+					//Verificacao se existe algum Fii apenas em arrayMeu que nao esta em arrayNovoMeu
+					var qtdAntigosFiisApenasMeus = (arrayMeu.length + novosFiisApenasDoRodrigo) - arrayRodrigo.length;
+					var antigosFiisApenasMeus = [];
+
+					if (qtdAntigosFiisApenasMeus > 0) {
+						for (var i = 0; i < arrayMeu.length; i++) {
+							if (!(tempArrayRodrigo).find(element => element==(arrayMeu[i]))) {
+								antigosFiisApenasMeus.push(arrayMeu[i]);
+							}
+						}
+					}
+
+					//Inserir antigosFiisApenasMeus no arrayRodrigo e em seguida dar um sort() nele
+					for (var i = 0; i < antigosFiisApenasMeus.length; i++) {
+						montadorDeLinha += antigosFiisApenasMeus[i];
+						montadorDeLinha += ";";
+						montadorDeLinha += "Venda";
+						montadorDeLinha += ";";
+						montadorDeLinha += "0.00%";
+						arrayNovoRodrigo.push(montadorDeLinha);
+						montadorDeLinha = "";
+					}
+					arrayRodrigo.sort()
+
+					//Ordenacao eh para poder inserir novosFiisApenasDoRodrigo no arrayMeu
+					novosFiisApenasDoRodrigo = novosFiisApenasDoRodrigo.sort();
+					var k=0; //iterador em [novosFiisApenasDoRodrigo]
+					var str1 = "AAAA11";
+					var str2 = "";
+					var str3 = "";
+					var s1;
+					var s2;
+					var s3;
+					for (var i = 0; i < (arrayMeu.length); i++) {
+						if (k<novosFiisApenasDoRodrigo.length) {
+							str2 = novosFiisApenasDoRodrigo[k];
+							str3 = arrayMeu[i];
+
+							s1=str1.toLowerCase().split("");
+						    s2=str2.toLowerCase().split("");
+						    s3=str3.toLowerCase().split("");
+						    var min = 4;//Math.min(...[s1.length, s2.length, s3.length]);
+
+						    var s1EstaNaOrdem = false;
+						    var s3EstaNaOrdem = false;
+						    var retorno = true;
+						    for(var j=0; j<min; j++) {
+						    	if (s1EstaNaOrdem && s3EstaNaOrdem) {
+						    		retorno = true;
+						    		break;
+						    	}
+						        if ((s1[j] > s2[j]) && !s1EstaNaOrdem) {
+						    		retorno = false;
+						    		break;
+						    	}
+						    	if ((s2[j] > s3[j]) && !s3EstaNaOrdem) {
+						    		retorno = false;
+						    		break;
+						    	}
+						    	if (s1[j] != s2[j]) {
+						    		s1EstaNaOrdem = true;
+						    	}
+						    	if (s2[j] != s3[j]) {
+						    		s3EstaNaOrdem = true;
+						    	}
+						    }
+
+							//TODO: Abaixo eh considerando que existe no max um alfabeticamente consecutivo dentro dos elementos de [novosFiisApenasDoRodrigo] no universo [novosFiisApenasDoRodrigo+arrayMeu]
+						    if (retorno) {
+								arrayNovoMeu.push("(-)");
+								k = k + 1;
+						    }
+						}
+					    arrayNovoMeu.push(arrayMeu[i]);
+						str1 = arrayMeu[i];
+					}
+
+					for (var i = k; i < novosFiisApenasDoRodrigo.length; i++) {
+						arrayNovoMeu.push(novosFiisApenasDoRodrigo[k]);
+					}
+					arrayMeu = arrayNovoMeu;
+
+				//Rodrigo Vendeu mais fundos do que Comprou ->   V > C
+				} else {
+
+					var arrayNovoRodrigo = [];
+					var iTeste = 0;
+					for (var i = 0; i < arrayMeu.length; i++) {
+						if ((arrayMeu[i] != "(-)") && (i<arrayRodrigo.length))  {
+
+							if ((tempArrayRodrigo).find(element => element==(arrayMeu[i]))) {
+								arrayNovoRodrigo.push(arrayRodrigo[i]);
+
+							} else {
+								iTeste = iTeste+1;
+								while (((i+iTeste) < arrayMeu.length) && !((tempArrayRodrigo).find(element => element==(arrayMeu[(i+iTeste)])))) {
+									iTeste = iTeste+1;
+								}
+
+								for (var j = 0; j < iTeste; j++) {
+									montadorDeLinha += "(-)";
+									montadorDeLinha += ";";
+									montadorDeLinha += "Venda";
+									montadorDeLinha += ";";
+									montadorDeLinha += "0.00%";
+									arrayNovoRodrigo.push(montadorDeLinha);
+									montadorDeLinha = "";
+								}
+								for (var j = 0; j < iTeste; j++) {
+									arrayNovoRodrigo.push(arrayRodrigo[(i+j)]);
+								}
+								i = i + (iTeste - 1);
+								iTeste = 0;
+							}
+						}
+					}
+					arrayRodrigo = arrayNovoRodrigo;
+
+				}
+
+				//Agora que ambos tem a mesma quantidade de elementos basta concatena-los
+				for (var i = 0; i < arrayRodrigo.length; i++) {
+					montadorDeLinha += arrayMeu[i];
+					montadorDeLinha += ";";
+					montadorDeLinha += arrayRodrigo[i];
+					arrayComAsDuasCarteiras.push(montadorDeLinha);
+					montadorDeLinha = "";
+				}
+			}
+
+		} else {
+			montadorDeLinha += "CARTEIRAS";
+			montadorDeLinha += ";";
+			montadorDeLinha += "IGUAIS";
+			montadorDeLinha += ";";
+			montadorDeLinha += "(-)";
+			montadorDeLinha += ";";
+			montadorDeLinha += "(-)";
+			arrayComAsDuasCarteiras.push(montadorDeLinha);
+			montadorDeLinha = "";
+			for (var i = 0; i < arrayRodrigo.length; i++) {
+				montadorDeLinha += arrayMeu[i];
+				montadorDeLinha += ";";
+				montadorDeLinha += arrayRodrigo[i];
+				arrayComAsDuasCarteiras.push(montadorDeLinha);
+				montadorDeLinha = "";
+			}
+		}
+		arrayRodrigo = arrayComAsDuasCarteiras;
+	}
+	
 
 
 
+	// console.log(orderedWords("AFHI11", "BARI11", "BLMR11"));
+	// console.log(orderedWords("BARI11", "AFHI11", "BLMR11"));
+	// console.log(orderedWords("AFHI11", "BLMR11", "BARI11"));
 
-
-
-	return (arr.join("\r\n"));
+	return (arrayRodrigo.join("\r\n"));
 }
 
+function orderedWords(str1, str2, str3) {
+    var s1=str1.toLowerCase().split("");
+    var s2=str2.toLowerCase().split("");
+    var s3=str3.toLowerCase().split("");
+    var min = Math.min(...[s1.length, s2.length, s3.length]);
 
+    var s1EstaNaOrdem = false;
+    var s3EstaNaOrdem = false;
+    for(var i=0; i<min; i++) {
+    	if (s1EstaNaOrdem && s3EstaNaOrdem) {
+    		return true;
+    	}
+        if ((s1[i] > s2[i]) && !s1EstaNaOrdem) {
+    		return false;
+    	}
+    	if ((s2[i] > s3[i]) && !s3EstaNaOrdem) {
+    		return false;
+    	}
+    	if (s1[i] != s2[i]) {
+    		s1EstaNaOrdem = true;
+    	}
+    	if (s2[i] != s3[i]) {
+    		s3EstaNaOrdem = true;
+    	}
+    }
+    return true;
+}
 
 
 
